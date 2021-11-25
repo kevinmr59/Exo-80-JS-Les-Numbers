3f32cb53736ddae089581554ae7366501f18e60f
kevinmr59 <kevinmura59610@gmail.com>
2021-11-25 19:29:07 +0100

    numbers

---
+++ exo.js
@@ -0,0 +1,39 @@
+let entier =document.getElementById('is-integer');
+if (Number.isInteger(entier)){
+    entier.innerHTML='est un nombre entier'
+}else {
+    entier.innerHTML= 'est pas un nombre entier'
+}
+
+let nombre = document.getElementById('is-nan');
+if (Number.isNaN(nombre)) {
+    nombre.innerHTML='est Nan';}
+    else
+    {
+        nombre.innerHTML='est un nombre';
+    }
+
+
+let flote =document.getElementById('parse-float');
+let floteO = parseInt(flote.innerHTML)
+flote.innerHTML= parseFloat(floteO*30)+'de la population blablabla';
+
+
+let four = document.getElementById('parse-int');
+let fourO = parseInt(four.innerHTML);
+four.innerHTML= parseInt(fourO/28)+ 'px';
+
+
+let five = document.getElementById('to-fixed');
+let fiveO =parseFloat(five.innerHTML);
+five.innerHTML=fiveO.toFixed(2);
+
+
+
+
+
+
+
+
+
+
